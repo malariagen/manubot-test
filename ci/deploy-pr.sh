95ec16ece839915e9d140e429ffef5640539657c
lee.hart@well.ox.ac.uk <4256466+leehart@users.noreply.github.com>
2020-06-14 19:31:04 +0000

    WIP: remove GH PR number env var echo in CI

--- ci/deploy-pr.sh
+++ ci/deploy-pr.sh
@@ -7,4 +7,5 @@ set -o errexit \
     -o nounset \
     -o pipefail
 
-echo >&2 "[INFO] WIP: This event is a pull request. SHA $GITHUB_PULL_REQUEST_SHA. Number $GIHUB_PULL_REQUEST_NUMBER."
+echo >&2 "[INFO] WIP: This event is a pull request."
+echo >&2 "[INFO] WIP: SHA $GITHUB_PULL_REQUEST_SHA"
